fbf272e5424009f33208017ccb906a0bf04b290d
Nicklas Markvart Nielsen <nicklas.m.nielsen@live.com>
2022-02-03 13:20:49 +0100

    5.1 - done

--- RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
+++ RealEstateApp/RealEstateApp/PropertyDetailPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using RealEstateApp.Models;
 using RealEstateApp.Services;
+using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using TinyIoC;
@@ -50,6 +52,46 @@ namespace RealEstateApp
             cts.Cancel();
             btnPlayText.IsEnabled = true;
             btnPauseText.IsEnabled = false;
+        }
+
+        private async void PhoneLabel_Tapped(object sender, System.EventArgs e)
+        {
+            try
+            {
+                string Choice = await DisplayActionSheet($"{Property.Vendor.FullName}", "Cancel", null, "Call", "SMS");
+                if (Choice == "Call")
+                {
+                    PhoneDialer.Open(Property.Vendor.Phone);
+                }
+                else if (Choice == "SMS")
+                {
+                    SmsMessage message = new SmsMessage($"Hej, {Property.Vendor.FullName}, angående {Property.Address}", Property.Vendor.Phone);
+                    await Sms.ComposeAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private async void MailLabel_Tapped(object sender, System.EventArgs e)
+        {
+            try
+            {
+                string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string attachmentFilePath = Path.Combine(folder, "property.txt");
+                File.WriteAllText(attachmentFilePath, $"{Property.Address}");
+
+                EmailMessage mail = new EmailMessage { Subject = "Real Estate" };
+                mail.Attachments.Add(new EmailAttachment(attachmentFilePath));
+
+                await Email.ComposeAsync(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
